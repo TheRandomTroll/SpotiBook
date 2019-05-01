e14f56c461d79a3d38f00a549db0d520bba5cb1f
A4ojo <jordanov.a4o@gmail.com>
2019-05-02 00:06:58 +0300

    Changed login/register so that they primarily use username instead of email

--- SpotiBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SpotiBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -40,6 +40,9 @@ namespace SpotiBook.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+
+            [Required]
+            public string Username { get; set; }
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -67,7 +70,7 @@ namespace SpotiBook.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
